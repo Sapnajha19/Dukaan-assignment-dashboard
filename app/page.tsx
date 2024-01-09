3041b5f2fa122f825f4439a35b4d61152cf154db
Sapna Jha <sapnajha1919@gmail.com>
2024-01-09 13:57:03 +0530

    Add Dashboard at index

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import Image from 'next/image'
+import Sidebar from './components/sidebar/Sidebar'
+import Navbar from './components/navigation/Navbar'
+import Dashboard from './components/dashboard/Dashboard'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
+    <div>
+    <Dashboard/>
+  </div>
   )
 }
